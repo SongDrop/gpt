a0e83cad928b5d393a566931d6f8daa61ca59b00
SongDrop Development <dev.team@songdrop.band>
2025-07-15 22:49:56 +0100

    working vector db

--- create_vm.py
+++ create_vm.py
@@ -145,22 +145,22 @@ async def main():
     REACT_APP_APP_NAME='AI Chat Assistant'
     REACT_APP_APP_LOGO='https://i.postimg.cc/C53CqTfx/chatgpt.png'
     VECTOR_SEARCH_ENABLED='true'
-    VECTOR_SEARCH_ENDPOINT='https://ragaisearchrtx.search.windows.net'
-    VECTOR_SEARCH_INDEX='rag-1752512002948'
+    VECTOR_SEARCH_ENDPOINT=''
+    VECTOR_SEARCH_INDEX=''
     VECTOR_SEARCH_KEY=''
     VECTOR_SEARCH_SEMANTIC_CONFIG='azureml-default'
     VECTOR_SEARCH_EMBEDDING_DEPLOYMENT='text-embedding-ada-002'
     VECTOR_SEARCH_EMBEDDING_ENDPOINT=''
     VECTOR_SEARCH_EMBEDDING_KEY=''
     # Storage for file upload\n\
-    VECTOR_SEARCH_STORAGE_ENDPOINT='https://ragstoragertx.blob.core.windows.net/'
+    VECTOR_SEARCH_STORAGE_ENDPOINT=''
     VECTOR_SEARCH_STORAGE_ACCESS_KEY=''
     VECTOR_SEARCH_STORAGE_CONNECTION_STRING=''
-    OPENAI_API_BASE="https://gabz-mb97c15u-swedencentral.cognitiveservices.azure.com/openai/deployments/gpt-4.1-mini/chat/completions?api-version=2025-01-01-preview"
+    OPENAI_API_BASE=""
     OPENAI_API_KEY=""
     OPENAI_DEPLOYMENT_NAME="gpt-4.1-mini"
     OPENAI_API_VERSION="2023-05-15"
-    GPT_IMAGE_URL="https://gabz-mbtgx2um-westus3.cognitiveservices.azure.com/openai/deployments/gpt-image-1/images"
+    GPT_IMAGE_URL=""
     GPT_IMAGE_KEY=""
     GPT_IMAGE_VERSION="2025-04-01-preview"
 
