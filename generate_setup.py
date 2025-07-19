f3c973e3ee651962f5dbd22c8998b643f01c3764
SongDrop Development <dev.team@songdrop.band>
2025-07-19 15:01:51 +0100

    fix ssl ws

--- generate_setup.py
+++ generate_setup.py
@@ -296,6 +296,19 @@ server {{
         proxy_request_buffering off;
         proxy_http_version 1.1;
     }}
+
+        location /chat {{
+        proxy_pass http://localhost:{BACKEND_PORT}/chat;
+        proxy_set_header Host \$host;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$connection_upgrade;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_buffering off;
+        proxy_request_buffering off;
+        proxy_http_version 1.1;
+    }}
     
      location /api {{
         proxy_pass http://localhost:{BACKEND_PORT}/api;
